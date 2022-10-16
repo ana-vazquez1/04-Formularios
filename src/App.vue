f3ba1ac3a053d1aeb20720c47c2628a83f6c14f9
ana-vazquez1 <a3521110459@alumno.uttehuacan.edu.mx>
2022-10-15 20:47:58 -0500

    Actualizado

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container mt-3">
+    <Header></Header>
+  <Formulario></Formulario>
+
+      
+</div>
+</template>
+
+<script>
+  import Header from "./components/Header.vue";
+  import Formulario from "./components/Formulario.vue";
+  import TotalProyectos from "./components/TotalProyectos.vue";
+  import ProgressBar from "./components/ProgressBar.vue";
+  export default{
+    components:{
+    Header,
+    Formulario,
+    TotalProyectos,
+    ProgressBar
+  },
+  };
+</script>
